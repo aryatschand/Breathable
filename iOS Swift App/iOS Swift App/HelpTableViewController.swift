8ad90f562a51be0c9e8a5e84867ac2dd2e0c0816
Arya Tschand <arya@voicesaver.com>
2019-09-14 15:48:14 -0400

    Graph import

--- iOS Swift App/iOS Swift App/HelpTableViewController.swift
+++ iOS Swift App/iOS Swift App/HelpTableViewController.swift
@@ -8,22 +8,81 @@
 import UIKit
 import Foundation
 import MessageUI
+import MapKit
+import CoreLocation
 
-class HelpTableViewController: UITableViewController, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate {
+class HelpTableViewController: UITableViewController, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate, CLLocationManagerDelegate {
 
     var names: [String] = ["WildLife", "Ocean"]
     var numbers: [String] = ["7325356202", "7324465525"]
     var emails: [String] = ["arya@voicesaver.com", "arya@gmail.com"]
+    var locManager = CLLocationManager()
+    var currentLocation: CLLocation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        printMessagesForUser()
+        locManager.requestWhenInUseAuthorization()
+        
+        if (CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedAlways){
+            guard let currentLocation = locManager.location else {
+                return
+            }
+            print(currentLocation.coordinate.latitude)
+            print(currentLocation.coordinate.longitude)
+            
+        }
+        
+        //let location = CLLocation(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude)
+        //var geocoder = CLGeocoder()
+        // Geocode Location
+        //geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            // Process Response
+            //self.processResponse(withPlacemarks: placemarks, error: error)
+        //}
     }
+    
+    var locationManager: CLLocationManager = CLLocationManager()
+    var latitude: Double = 0
+    var longitude: Double = 0
+    var addressString : String = ""
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    func printMessagesForUser() -> Void {
+            let json = ["user":"larry"]
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+                
+                let url = NSURL(string: "https://h2hacks1.herokuapp.com/api")!
+                let request = NSMutableURLRequest(url: url as URL)
+                request.httpMethod = "POST"
+                
+                request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+                request.httpBody = jsonData
+                
+                let task = URLSession.shared.dataTask(with: request as URLRequest){ data, response, error in
+                    if error != nil{
+                        print("Error -> \(error)")
+                        return
+                    }
+                    do {
+                        let result = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String:AnyObject]
+                        print("Result -> \(result)")
+                        
+                    } catch {
+                        print("Error -> \(error)")
+                    }
+                }
+                
+                task.resume()
+            } catch {
+                print(error)
+            }
+        }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
@@ -64,4 +123,30 @@ class HelpTableViewController: UITableViewController, MFMailComposeViewControlle
         self.present(composeVC, animated: true, completion: nil)
     }
 
+}
+
+extension CLPlacemark {
+    
+    var compactAddress: String? {
+        if let name = name {
+            var result = name
+            
+            if let street = thoroughfare {
+                result += ", \(street)"
+            }
+            
+            if let city = locality {
+                result += ", \(city)"
+            }
+            
+            if let country = country {
+                result += ", \(country)"
+            }
+            
+            return result
+        }
+        
+        return nil
+    }
+    
 }
